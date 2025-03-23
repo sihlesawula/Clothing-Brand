0326079a8d91d301abc331ec97a78347de496ba7
sihlesawula <150780503+sihlesawula@users.noreply.github.com>
2025-03-23 22:39:51 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-
+// Function to toggle the menu on smaller devices
+function toggleMenu() {
+    const navbarLinks = document.querySelector('.navbar-links');
+    navbarLinks.classList.toggle('active');
+}
